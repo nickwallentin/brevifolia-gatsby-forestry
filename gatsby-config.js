c30eeb258e71c458b71f1fe5fb64927af221c8e4
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-05-08 18:34:19 +0200

    fix insta

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
     {
       resolve: `gatsby-source-instagram`,
       options: {
-        username: `hakulforfan`,
+        username: `lenabergkvist`,
       },
     },
     `gatsby-transformer-sharp`,
